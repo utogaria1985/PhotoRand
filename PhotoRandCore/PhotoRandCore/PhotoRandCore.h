2be8137f4c27f521d9f8bbb82ec1d391a07e5b4a
Jing Yu <arigatou1985@icloud.com>
2020-05-28 14:31:07 +0200

    Setup workspace and project dependency
    - Created a Xcode workspace and a framework project.
    - Use PhotoRandCore framework as a module that provides core functionalities to the PhotoRand app.

---
+++ PhotoRandCore/PhotoRandCore/PhotoRandCore.h
@@ -0,0 +1,19 @@
+//
+//  PhotoRandCore.h
+//  PhotoRandCore
+//
+//  Created by Jing Yu on 2020-05-28.
+//  Copyright © 2020 Jing Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhotoRandCore.
+FOUNDATION_EXPORT double PhotoRandCoreVersionNumber;
+
+//! Project version string for PhotoRandCore.
+FOUNDATION_EXPORT const unsigned char PhotoRandCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoRandCore/PublicHeader.h>
+
+
